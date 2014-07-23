16d5bf8af3e4d370f31f83ffd78ac0a2ca237829
Fabian Harth <fabian.harth@novatec-gmbh.de>
2014-07-23 17:32:57 +0200

    DIE TESTS LAUFEN JETZT ENDLICH

--- src/test/float/float_test_makefile.mk
+++ src/test/float/float_test_makefile.mk
@@ -4,4 +4,4 @@ FLOAT_SOURCE_LOCATIONS=
 include ../src/generated/float/float_makefile.mk
 
 float: $(FLOAT_DEPENDENCIES)
-	$(CC) -o float_test $(FLOAT_DEPENDENCIES) ../src/test/float/float_test.o -I/usr/include/glib-2.0 -I/usr/lib/x86_64-linux-gnu/glib-2.0/include $(FLOAT_SOURCE_LOCATIONS) -lglib-2.0 -lcheck -lm -lpthread -lrt
+	$(CC) -o float_test $(FLOAT_DEPENDENCIES) ../src/test/float/float_test.c $(FLOAT_SOURCE_LOCATIONS) $(CFLAGS)
