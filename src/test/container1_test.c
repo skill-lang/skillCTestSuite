ebed423e8aae4e78c751972cdf1413e49068e6c2
Fabian Harth <fabian.harth@novatec-gmbh.de>
2014-11-05 16:32:54 +0100

    fixed reading several blocks

--- src/test/container1_test.c
+++ src/test/container1_test.c
@@ -129,7 +129,35 @@ char *write_read_instances () {
     return 0;
 }
 
+// This test just reads a bunch of binary files to check, that the parsing does not cause errors.
+char *generic_parse_test () {
+    skill_state state = skill_state_from_file ( "./resources/working/coloredNodes.sf" );
+    delete_skill_state ( state );
+    state = skill_state_from_file ( "./resources/working/date.sf" );
+    delete_skill_state ( state );
+    state = skill_state_from_file ( "./resources/working/emptyBlocks.sf" );
+    delete_skill_state ( state );
+    state = skill_state_from_file ( "./resources/working/fourColoredNodes.sf" );
+    delete_skill_state ( state );
+    state = skill_state_from_file ( "./resources/working/localBasePoolStartIndex.sf" );
+    delete_skill_state ( state );
+    state = skill_state_from_file ( "./resources/working/node.sf" );
+    delete_skill_state ( state );
+    state = skill_state_from_file ( "./resources/working/noFieldRegressionTest.sf" );
+    delete_skill_state ( state );
+    state = skill_state_from_file ( "./resources/working/trivialType.sf" );
+    delete_skill_state ( state );
+    state = skill_state_from_file ( "./resources/working/twoNodeBlocks.sf" );
+    delete_skill_state ( state );
+    state = skill_state_from_file ( "./resources/working/twoTypes.sf" );
+    delete_skill_state ( state );
+    state = skill_state_from_file ( "./resources/working/unicode-reference.sf" );
+    delete_skill_state ( state );
+    return 0;
+}
+
 static char *all_tests () {
+    mu_run_test ( generic_parse_test );
     mu_run_test ( write_read_instances );
     return 0;
 }
